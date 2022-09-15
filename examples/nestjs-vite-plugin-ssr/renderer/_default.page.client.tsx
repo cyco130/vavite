8336f688097c9792c1a4c4ab401e2301c78fa6e0
cyco130 <cyco130@gmail.com>
2022-09-15 12:56:06 +0300

    chore: fix type errors in examples

--- examples/nestjs-vite-plugin-ssr/renderer/_default.page.client.tsx
+++ examples/nestjs-vite-plugin-ssr/renderer/_default.page.client.tsx
@@ -1,7 +1,6 @@
 import { Root, hydrateRoot, createRoot } from "react-dom/client";
 import type { PageContextBuiltInClient } from "vite-plugin-ssr/client/router";
 import type { PageContext } from "./types";
-import { ReactStreaming } from "react-streaming/client";
 import { PageWrapper } from "./PageWrapper";
 
 export { render };
@@ -12,11 +11,9 @@ let root: Root | null = null;
 async function render(pageContext: PageContextBuiltInClient & PageContext) {
 	const { Page, pageProps } = pageContext;
 	const page = (
-		<ReactStreaming>
-			<PageWrapper pageContext={pageContext}>
-				<Page {...pageProps} />
-			</PageWrapper>
-		</ReactStreaming>
+		<PageWrapper pageContext={pageContext}>
+			<Page {...pageProps} />
+		</PageWrapper>
 	);
 	const container = document.getElementById("page-view")!;
 	if (container.innerHTML === "" || !pageContext.isHydration) {
