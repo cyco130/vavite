80a16d938b4813ea21cb0923c654f2b6986ba331
Fatih Aygün <cyco130@gmail.com>
2023-07-12 09:48:07 +0100

    chore: update core dependencies

--- examples/vite-plugin-ssr/vite.config.ts
+++ examples/vite-plugin-ssr/vite.config.ts
@@ -11,19 +11,7 @@ export default defineConfig({
 		{
 			name: "server",
 			config: {
-				build: {
-					ssr: true,
-					rollupOptions: {
-						output: {
-							// We have to disable this for multiple entries
-							inlineDynamicImports: false,
-							// vite-plugin-ssr overrides this, making it hard to find the entry file
-							entryFileNames() {
-								return "[name].js";
-							},
-						},
-					},
-				},
+				build: { ssr: true },
 			},
 		},
 	],
