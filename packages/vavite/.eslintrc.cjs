a8535b3e3a77941daa27853c4d10a7485aa4a4bd
Fatih Aygün <cyco130@gmail.com>
2024-08-10 23:11:51 +0100

    fix: update dependencies (#111)

--- packages/vavite/.eslintrc.cjs
+++ packages/vavite/.eslintrc.cjs
@@ -2,7 +2,7 @@ require("@cyco130/eslint-config/patch");
 
 module.exports = {
 	extends: ["@cyco130/eslint-config/node"],
-	ignorePatterns: ["dist", "node_modules", "**/*.cjs"],
+	ignorePatterns: ["dist", "node_modules", "**/*.cjs", "cli.js"],
 	parserOptions: { project: __dirname + "/tsconfig.json" },
 	rules: {
 		"import/no-unresolved": [
