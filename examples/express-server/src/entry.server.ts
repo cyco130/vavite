0a02a9e7e36aa8b8bbca3424340e0387a2e6dc03
Fatih Aygün <cyco130@gmail.com>
2026-04-13 18:46:15 +0100

    feat: dispose on server restart

--- examples/express-server/src/entry.server.ts
+++ examples/express-server/src/entry.server.ts
@@ -6,11 +6,18 @@ import barRoute from "./routes/bar";
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+	next();
+});
+
 app.get("/", homeRoute);
 app.get("/foo", fooRoute);
 app.get("/bar", barRoute);
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+console.log("Starting server...");
 const server = app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
